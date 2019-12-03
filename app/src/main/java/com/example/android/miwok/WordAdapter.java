e27df9fb6cec7a47d4d3bb817fc4432520c1b736
Aarushi VIj <aarushivij@gmail.com>
2019-12-03 21:49:32 +0530

    visiual polish

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -4,19 +4,28 @@ import android.content.Context;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class WordAdapter extends ArrayAdapter<Word>{
+public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Context context, ArrayList<Word> pWords) {
-        super(context,0, pWords);
+
+    int mcolorResourceId;
+
+    public WordAdapter(Context context, ArrayList<Word> pWords, int colorResourceId) {
+
+
+        super(context, 0, pWords);
+        mcolorResourceId = colorResourceId;
     }
 
 
@@ -24,14 +33,14 @@ public class WordAdapter extends ArrayAdapter<Word>{
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
         View listItemView = convertView;
-        if(listItemView == null) {
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
 
         Word word = getItem(position);
         //Finding the text view for english word
-        TextView english_word  = (TextView) listItemView.findViewById(R.id.english_word);
+        TextView english_word = (TextView) listItemView.findViewById(R.id.english_word);
         //get english word from word object and set this text to english_word textview
         english_word.setText(word.getmEnglishWord());
 
@@ -42,19 +51,20 @@ public class WordAdapter extends ArrayAdapter<Word>{
         miwok_word.setText(word.getmMiwokWord());
 
 
-                //finding imageview
-                ImageView image = (ImageView) listItemView.findViewById(R.id.imageview);
-                if(word.imagepresent()) {
-                    //getting image from word object and setting this to imageres
-                    image.setImageResource(word.getmIageres());
-                    image.setVisibility(View.VISIBLE);
-                }
-                else
-                {
-                    image.setVisibility(View.GONE);
-                }
-        return listItemView;
+        //finding imageview
+        ImageView image = (ImageView) listItemView.findViewById(R.id.imageview);
+        if (word.imagepresent()) {
+            //getting image from word object and setting this to imageres
+            image.setImageResource(word.getmIageres());
+            image.setVisibility(View.VISIBLE);
+        } else {
+            image.setVisibility(View.GONE);
+        }
 
+        View textContainer = (View) listItemView.findViewById(R.id.text_root_view);
+        int color = ContextCompat.getColor(getContext(),mcolorResourceId);
+        textContainer.setBackgroundColor(color);
+        return listItemView;
 
 
     }
